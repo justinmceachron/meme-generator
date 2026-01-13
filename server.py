04cee93539372b80acfe3f9b5b26c5f4e721e767
justinmceachron <139165673+justinmceachron@users.noreply.github.com>
2026-01-13 12:45:17 -0600

    Initial commit: Meme Generator web app

---
+++ server.py
@@ -0,0 +1,18 @@
+import http.server
+import socketserver
+import os
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        return super().do_GET()
+
+PORT = 8000
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+with socketserver.TCPServer(("", PORT), MyHTTPRequestHandler) as httpd:
+    print(f"Server running at http://localhost:{PORT}/")
+    print("Press Ctrl+C to stop the server")
+    httpd.serve_forever()
